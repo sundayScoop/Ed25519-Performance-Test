0e7a7572d6e8adcd06bad758fff45b300776e1a3
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2022-09-14 22:32:27 +1000

    using bouncy castle now

--- Program.cs
+++ Program.cs
@@ -1,22 +1,15 @@
 ﻿using System.Collections;
-using System.Drawing;
-using System.Net;
-using System.Numerics;
 using System.Security.Cryptography;
-
+using Org.BouncyCastle.Math;
 class Point
 {
     public BigInteger X { get; } // Change to private
     public BigInteger Y { get; }
-    public BigInteger Z { get; }
-    public BigInteger T { get; }
 
-    public Point(BigInteger x, BigInteger y, BigInteger z, BigInteger t)
+    public Point(BigInteger x, BigInteger y)
     {
-        X = x;
-        Y = y;
-        Z = z;
-        T = t;
+        this.X = x;
+        this.Y = y;
     }
 
     // Using double and add algorithm
@@ -24,121 +17,87 @@ class Point
     {
         // TODO: Check to see if 0 < num < N
 
-        Point newPoint = new Point(BigInteger.Zero, BigInteger.One, BigInteger.One, BigInteger.Zero);
+        Point newPoint = new Point(BigInteger.Zero, BigInteger.One);
 
-
-        while (num > BigInteger.Zero)
+        while (num.CompareTo(BigInteger.Zero) > 0)
         {
-            if ((num & BigInteger.One).Equals(BigInteger.One))
-            {
-                newPoint = Add(newPoint, point);
-                Console.WriteLine("Add");
-            }
-            point = Double(point);
-            Console.WriteLine("Double");
-            num = num >> 1;
-            Console.WriteLine(num.ToString());
+            if ((num.And(BigInteger.One)).Equals(BigInteger.One)) newPoint = newPoint + point;
+            point = point + point;
+            num = num.ShiftRight(1);
         }
         return newPoint;
     }
-    public static Point Double(Point point)
-    {
-        BigInteger A = Mod(point.X * point.X);
-        BigInteger B = Mod(point.Y * point.Y);
-        BigInteger C = Mod(BigInteger.Parse("2") * Mod(point.Z * point.Z));
-        BigInteger D = Mod(BigInteger.MinusOne * A);
-        BigInteger x1y1 = point.X + point.Y;
-        BigInteger E = Mod(Mod(x1y1 * x1y1) - A - B);
-        BigInteger G = D + B;
-        BigInteger F = G - C;
-        BigInteger H = D - B;
-        BigInteger X3 = Mod(E * F);
-        BigInteger Y3 = Mod(G * H);
-        BigInteger T3 = Mod(E * H);
-        BigInteger Z3 = Mod(F * G);
-        return new Point(X3, Y3, Z3, T3);
-    }
-    public static Point Double2(Point point)
-    {
-        BigInteger A = Mod(point.X * point.X);
-        BigInteger B = Mod(point.Y * point.Y);
-        BigInteger D = Mod(BigInteger.MinusOne * A);
-        BigInteger x1y1 = point.X + point.Y;
-        BigInteger E = Mod(Mod(x1y1 * x1y1) - A - B);
-        BigInteger G = D + B;
-        BigInteger H = D - B;
-        BigInteger X3 = Mod(E * (G-2));
-        BigInteger Y3 = Mod(G * H);
-        BigInteger T3 = Mod(E * H);
-        BigInteger Z3 = Mod(Mod(G * G) - (2 * G));
-        return new Point(X3, Y3, Z3, T3);
-    }
-    public static Point Add(Point point1, Point point2)
+
+    public static Point operator +(Point point1, Point point2)
     {
         // TODO: check to see if point is on curve and on the prime order subgroup
+        // https://bibliotecadigital.ipb.pt/bitstream/10198/24067/1/Nakai_Eduardo.pdf Page 20. I added finite fields
 
-        
-        BigInteger A = Mod((point1.Y - point1.X) * (point2.Y + point2.X));
-        BigInteger B = Mod((point1.Y + point1.X) * (point2.Y - point2.X));
-        BigInteger F = Mod(B - A);
-        if (F.Equals(BigInteger.Zero))
-        {
-            Console.WriteLine("here");
-            return Double(point1); // Same point.
-        }
-        BigInteger C = Mod(point1.Z * BigInteger.Parse("2") * point2.T);
-        BigInteger D = Mod(point1.T * BigInteger.Parse("2") * point2.Z);
-        BigInteger E = D + C;
-        BigInteger G = B + A;
-        BigInteger H = D - C;
-        BigInteger X3 = Mod(E * F);
-        BigInteger Y3 = Mod(G * H);
-        BigInteger T3 = Mod(E * H);
-        BigInteger Z3 = Mod(F * G);
-        return new Point(X3, Y3, Z3, T3);
+
+        return new Point(
+            // (   ( X1   *         Y2     +      Y1       *        X2   )   *     (    1          +  (     D         *       X1         *       X2        *        Y1        *         Y2  ) )  ^                M - 2                   % M       )    % M         
+            ((point1.X.Multiply(point2.Y).Add(point1.Y.Multiply(point2.X))).Multiply((BigInteger.One.Add(Ed25519.D.Multiply(point1.X).Multiply(point2.X).Multiply(point1.Y).Multiply(point2.Y))).ModPow(Ed25519.M.Subtract(BigInteger.Two), Ed25519.M))).Mod(Ed25519.M),
+            // (   ( Y1   *         Y2         -     (      A       *        X1      *         X2  ) )    *      (       1         -          D       *       X1        *       X2        *        Y1        *         Y2  )    ^                M - 2                      % M       )    % M     
+            ((point1.Y.Multiply(point2.Y).Subtract(Ed25519.A.Multiply(point1.X).Multiply(point2.X))).Multiply((BigInteger.One.Subtract(Ed25519.D.Multiply(point1.X).Multiply(point2.X).Multiply(point1.Y).Multiply(point2.Y))).ModPow(Ed25519.M.Subtract(BigInteger.Two), Ed25519.M))).Mod(Ed25519.M)
+            );
     }
 
-    private static BigInteger Mod2(BigInteger a)
-    {
-        BigInteger n = a % Ed25519.M; return n < 0 ? n + Ed25519.M : n;
-    }
-    private static BigInteger Mod(BigInteger a)
-    {
-        //Console.WriteLine(a.ToString());
-        BigInteger res = a % Ed25519.M;
-        return res >= BigInteger.Zero ? res : Ed25519.M + res;
-    }
 }
 
 readonly struct Ed25519
 {
-    public static BigInteger M = BigInteger.Parse("57896044618658097711785492504343953926634992332820282019728792003956564819949");
-    public static BigInteger N = BigInteger.Parse("7237005577332262213973186563042994240857116359379907606001950938285454250989");
-    public static BigInteger D = BigInteger.Parse("37095705934669439343138083508754565189542113879843219016388785533085940283555"); // -121665 * pow(121666, -1, 2**255 - 19)
-    public static BigInteger A = BigInteger.MinusOne;
-    public static BigInteger Gx = BigInteger.Parse("15112221349535400772501151409588531511454012693041857206046113283949847762202");
-    public static BigInteger Gy = BigInteger.Parse("46316835694926478169428394003475163141307993866256225615783033603165251855960");
-    public static BigInteger Gt = BigInteger.Parse("46827403850823179245072216630277197565144205554125654976674165829533817101731");
-    public static Point G = new Point(Gx, Gy, BigInteger.One, Gt);
+    public static BigInteger M = new BigInteger("57896044618658097711785492504343953926634992332820282019728792003956564819949", 10);
+    public static BigInteger N = new BigInteger("7237005577332262213973186563042994240857116359379907606001950938285454250989", 10);
+    public static BigInteger D = new BigInteger("-4513249062541557337682894930092624173785641285191125241628941591882900924598840740", 10); // -121665 * pow(121666, -1, 2**255 - 19)
+    public static BigInteger A = new BigInteger("-1", 10);
+    public static BigInteger Gx = new BigInteger("15112221349535400772501151409588531511454012693041857206046113283949847762202", 10);
+    public static BigInteger Gy = new BigInteger("46316835694926478169428394003475163141307993866256225615783033603165251855960", 10);
+
+    public static Point G = new Point(Gx, Gy);
 
 }
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        //var point = Point.Add(Ed25519.G, Ed25519.G);
-        //var p2 = Ed25519.G * BigInteger.Parse("4");
-        Point l0 = new Point(BigInteger.Zero, BigInteger.One, BigInteger.One, BigInteger.Zero);
+        int i = 0;
+        while (i < 10)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // random bytes for h, priv and r
+            byte[] r_b = RandomNumberGenerator.GetBytes(32);
+            byte[] h_b = RandomNumberGenerator.GetBytes(64);
+            byte[] priv_b = RandomNumberGenerator.GetBytes(32);
 
-        var p2 = Point.Double(Ed25519.G);
+            // bytes to numbers
+            BigInteger priv = new BigInteger(priv_b).Mod(Ed25519.N); // Mod N to ensure size constraint
+            BigInteger r = new BigInteger(r_b).Mod(Ed25519.N);
+            BigInteger h = new BigInteger(h_b).Mod(Ed25519.N);
 
-        Console.WriteLine(p2.X.ToString());
+
+            Point R = Ed25519.G * r;
+            BigInteger s = (r.Add(h.Multiply(priv))).Mod(Ed25519.N);
+            watch.Stop();
+            Console.Write($"Total Sign Time: {watch.ElapsedMilliseconds} ms");
+
+            var pub = Ed25519.G * priv;
+            watch.Start();
+            
+            ///// Verifying
+            var point1 = Ed25519.G * s;
+            var point2 = R + ((pub) * h);
+
+
+            bool valid = point1.X.Equals(point2.X);
+
+            watch.Stop();
+            Console.Write($"Total Verify Time: {watch.ElapsedMilliseconds} ms");
+            Console.WriteLine(valid);
+            i += 1;
+        }
+
     }
-    private static BigInteger Mod(BigInteger a, BigInteger p) 
-    {
-        BigInteger m = a % p; return m < 0 ? m + p : m; 
-    }
+    
 
 }
